0735f9a1a8c3c3f15670a9b17cce3af27a30276a
PhilWinchester <pwinches@conncoll.edu>
2016-09-18 12:48:51 -0400

    creating board with js

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,20 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-if (cardTwo === cardFour){
-  alert("You found a match!");
-} else{
-  alert("Sorry, try again");
+// if (cardTwo === cardFour){
+//   alert("You found a match!");
+// } else{
+//   alert("Sorry, try again");
+// }
+
+var gameboard = document.getElementById('game-board');
+
+function createBoard(){
+  for (var i = 0; i < 4; i++){
+    var tempCard = document.createElement('div');
+    tempCard.className = "card";
+    gameboard.appendChild(tempCard);
+  }
 }
+
+createBoard();

d4034d00a6f3745fc4a637346808ebdcdeccd228
PhilWinchester <pwinches@conncoll.edu>
2016-09-18 21:35:20 -0400

    finished v1 of game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,21 +1,36 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-
-// if (cardTwo === cardFour){
-//   alert("You found a match!");
-// } else{
-//   alert("Sorry, try again");
-// }
+var cards = ['queen', 'queen','king','king'];
+var cardsInPlay = [];
 
 var gameboard = document.getElementById('game-board');
 
 function createBoard(){
-  for (var i = 0; i < 4; i++){
-    var tempCard = document.createElement('div');
-    tempCard.className = "card";
-    gameboard.appendChild(tempCard);
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('div');
+    cardElement.className = "card";
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click',isTwoCards);
+    gameboard.appendChild(cardElement);
+  }
+}
+
+function isMatch(temp){
+  if(temp[0] === temp[1]){
+    document.getElementById('message').textContent = "Winner!!";
+  } else {
+    document.getElementById('message').textContent = "Better luck next time";
+  }
+}
+
+function isTwoCards(){
+  cardsInPlay.push(this.getAttribute('data-card'));
+  if (this.getAttribute('data-card') === 'king'){
+    this.innerHTML = '<img src="king.png" alt="King of Diamonds"/>';
+  } else if (this.getAttribute('data-card') === 'queen'){
+    this.innerHTML = '<img src="queen.png" alt="Queen of Diamonds"/>';
+  }
+  if (cardsInPlay.length === 2){
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
   }
 }
 
